e9309dc21b6625f9b71b32005982e71db601a617
Matthias Rohr <rohrmat@gmail.com>
2026-09-12 10:21:10 +0200

    build: pin the aiscb-0.1.15 bundle

--- setup.sh
+++ setup.sh
@@ -28,9 +28,9 @@ command -v sha256sum >/dev/null 2>&1 || {
 
 # This tag is never moved or reused. The hashes keep a moved or corrupted tag
 # from changing what this reviewed bootstrap executes.
-bundle_ref="aiscb-bundle-0.1.14-10"
-baseline_sha="a6fc88833aaae7f9e5e1ff9ebe4ba152e56660e743c75094b56b8b25968bfeb2"
-installer_sha="2f5f3dbdb28a7b68f39f188d6ddd72bc48b6ae35632e11a702ac9e3794014c5b"
+bundle_ref="aiscb-bundle-0.1.15-1"
+baseline_sha="d6a56f616f2b701efb160cb902c41318daf0b6ee113adba79ef294a6740c6ea2"
+installer_sha="ac0689a285f199317efe0b594b39f07e66ea0dc9c32a666306ee64ab7c67df04"
 helper_sha="b2fa3d5d1d9d891117ca9b035db243129d24b6eb0c2c54c3568eef623f83bdea"
 
 setup_tmp=$(mktemp -d "${TMPDIR:-/tmp}/aiscb-setup.XXXXXX")
